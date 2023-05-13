4307aa51f23e099b53280faa4fe65827fa17a284
liucy1898 <liucy1898@hotmail.com>
2023-05-13 14:47:07 +0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using demo;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var engine = new engine();
+        var rules = "A->B->C";
+
+        engine.parseRule(rules);
+
+        engine.fire();
+
+    }
+}
---
+++ src/engine.cs
@@ -0,0 +1,108 @@
+
+namespace demo
+{
+
+    class engine
+    {
+
+        List<task> tasks = new List<task>();
+
+        task? currentTask = null;
+
+
+        public void parseRule(string rule)
+        {
+
+            var taskStrings = rule.Split("->");
+
+            for (int i = 0; i < taskStrings.Length; i++)
+            {
+                tasks.Add(new task(this) { taskId = i, taskName = taskStrings[i] });
+            }
+
+        }
+
+        public void notify(taskState state)
+        {
+            Console.WriteLine($"current state {state.status}");
+            if (state.status == status.error)
+            {
+                Console.WriteLine($"this task {@state.taskId}, be {@state.status}");
+            }
+            else
+            {
+                try
+                {
+                    currentTask = tasks[state.taskId + 1];
+
+                    currentTask.execute();
+                }
+                catch (System.Exception)
+                {
+
+                    Console.WriteLine("no any task");
+                }
+
+
+
+
+            }
+        }
+
+        public void fire()
+        {
+            if (currentTask == null)
+            {
+                currentTask = tasks[0];
+
+                currentTask.execute();
+            }
+        }
+    }
+
+    enum status
+    {
+        done,
+        error
+    }
+
+    class taskState
+    {
+        public status status { get; set; }
+        public int taskId { get; set; }
+    }
+
+    class task
+    {
+
+        private engine taskEngine;
+
+        public task(engine engine)
+        {
+            taskEngine = engine;
+        }
+
+        public int taskId { get; set; }
+        public string? taskName { get; set; }
+
+        public void execute()
+        {
+
+            try
+            {
+                Console.WriteLine($"task {this.taskName}, is running");
+                //throw new Exception();
+                taskEngine.notify(new taskState() { taskId = this.taskId, status = status.done });
+            }
+            catch (System.Exception)
+            {
+                taskEngine.notify(new taskState() { taskId = this.taskId, status = status.error });
+
+            }
+
+
+        }
+
+    }
+
+}
